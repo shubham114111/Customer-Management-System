d5dcfb4f505dbc50cc44c31a175b5145839a48e9
shubham114111 <sshubham681@gmail.com>
2021-02-18 12:25:48 +0530

    Dyanamic Urls!

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('product/',views.product, name='product'),
-    path('customer/',views.customer, name='customer'),
+    path('customer/<int:pk>/',views.customer, name='customer'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,30 @@
+from accounts.models import Customer, Order, Product
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'accounts/home.html')
+    orders = Order.objects.all()
+    customers =Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status = 'Delivered').count()
+    pending = orders.filter(status = 'Pending').count()
+
+    context ={'orders':orders, 'customers': customers,
+    'total_orders':total_orders, 'delivered':delivered,
+    'pending':pending
+    }
+
+    return render(request, 'accounts/home.html',context)
 
 def product(request):
-    return render(request, 'accounts/product.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/product.html', {'products':products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    total_orders = orders.count()
+    context = {'customer':customer, 'orders':orders, 'total_orders':total_orders}
+
+    return render(request, 'accounts/customer.html',context)
